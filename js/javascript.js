7d78341c5f60389ef1e4784dd3d4d809eecaf408
Teferson.L <tefersonluan@gmail.com>
2023-01-17 20:13:04 -0300

    aff

--- js/javascript.js
+++ js/javascript.js
@@ -1,39 +1,3 @@
-/**
- * Teste do menu
- */
-
-
-/*
-const menuButton = document.getElementById('menu-button');
-const menu = document.getElementById('nav');
-
-menuButton.addEventListener('click', toggleMenu);
-
-menuButton.style.display = 'none';
-
-let resizeTimer;
-window.onresize = function() {
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(checkScreenSize, 170);
-};
-function checkScreenSize() {
-    if (window.innerWidth < 600) {
-        menuButton.style.display = 'block';
-        menu.style.display = 'none';
-    } else {
-        menuButton.style.display = 'none';
-        menu.style.display = 'block';
-    }
-}
-
-function toggleMenu() {
-    if (menu.style.display === 'none') {
-        menu.style.display = 'block';
-    } else {
-        menu.style.display = 'none';
-    }
-}
-*/
 
 /* Animação de carregamento */
 const observer = new IntersectionObserver((entries) => {
@@ -53,4 +17,3 @@ const observer = new IntersectionObserver((entries) => {
 
 const HiddenElements = document.querySelectorAll('.hidden')
 HiddenElements.forEach((el) => observer.observe(el))
-
