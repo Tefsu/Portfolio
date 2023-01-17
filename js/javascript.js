4f2d07a1c23cde3472678b11f570e8afa2a24e0b
Teferson.L <tefersonluan@gmail.com>
2023-01-17 19:10:22 -0300

    skills att

--- js/javascript.js
+++ js/javascript.js
@@ -37,19 +37,20 @@ function toggleMenu() {
 
 /* Animação de carregamento */
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) =>{
-    if(entry.isIntersecting){
-        entry.target.classList.add('show')
-    } else {entry.target.classList.remove('show')}
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else { entry.target.classList.remove('show') }
 
-    /*
-    ATENÇÃO
-
-    Para fazer o efeito sempre, adicionar um else
-    e dentro dele, colocar entry.target.classList.remove('show')
-    */
-})
+        /*
+        ATENÇÃO
+    
+        Para fazer o efeito sempre, adicionar um else
+        e dentro dele, colocar entry.target.classList.remove('show')
+        */
+    })
 })
 
 const HiddenElements = document.querySelectorAll('.hidden')
 HiddenElements.forEach((el) => observer.observe(el))
+
