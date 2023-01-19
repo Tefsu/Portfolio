2f4218ec4f8339a4be765ebb8722e5cdc2a7a41f
Teferson.L <tefersonluan@gmail.com>
2023-01-19 17:45:20 -0300

    behavior-smooth

--- js/javascript.js
+++ js/javascript.js
@@ -17,3 +17,26 @@ const observer = new IntersectionObserver((entries) => {
 
 const HiddenElements = document.querySelectorAll('.hidden')
 HiddenElements.forEach((el) => observer.observe(el))
+
+var links = document.querySelectorAll('a[href^="#"]');
+
+for(var i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function(e) {
+        e.preventDefault();
+
+        var target = this.getAttribute('href');
+        var section = document.querySelector(target);
+
+        var top = section.offsetTop;
+
+        window.scrollTo({
+            top: top,
+            behavior: 'smooth'
+        });
+
+        for(var j = 0; j < links.length; j++) {
+            links[j].classList.remove('active');
+        }
+        this.classList.add('active');
+    });
+}
