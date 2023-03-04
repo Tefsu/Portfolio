cd9e3fc06385c70d9e938b1701836626d10b8d72
Teferson <tefersonluan@gmail.com>
2023-03-04 20:38:29 -0300

    Creating projetos carousel

--- js/javascript.js
+++ js/javascript.js
@@ -40,3 +40,16 @@ for(var i = 0; i < links.length; i++) {
         this.classList.add('active');
     });
 }
+
+const slides = document.querySelectorAll('.carousel-slide');
+let currentSlide = 0;
+
+function showSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+setInterval(showSlide, 3000);
+
+
